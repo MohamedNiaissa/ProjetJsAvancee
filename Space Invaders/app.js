99317f9e4bd6389f16819fec66e59fc3f84ce85a
Mohamed <mohameddrame907@gmail.com>
2022-01-04 22:03:02 +0100

    Ajout déplacement joueur

--- Space Invaders/app.js
+++ Space Invaders/app.js
@@ -2,7 +2,13 @@ class GridPatern {
 
     constructor() {
         this.mov = 0;
+        this.boolAvance = true; 
+        this.boolLeft = true;
+        this.boolRightt = true;
+
+
     }
+
 
     patern = [1,2,3,4,5,8,9,10,11,12,16,18];
 
@@ -13,7 +19,8 @@ class GridPatern {
         const row = 9;
         const col = 7
 
-        for(let i = 0; i < row*col; i++) {
+
+        for(let i = 0; i < row*col; i++) {              //Create the cases
             let gridCase = document.createElement('div');
             gridCase.setAttribute("id", "case");
             container.appendChild(gridCase);
@@ -22,37 +29,117 @@ class GridPatern {
         this.handleEnemiesPatern();
     }
 
+    handlePlayerPatern(){
+        const gridCase = document.querySelectorAll("#case");
+
+        let playerShip = document.createElement('img');
+        playerShip.setAttribute("src", "./ressources/vaisseau.png");
+        
+
+        let position = 59;
+        gridCase[position].appendChild(playerShip);
+
+
+        document.addEventListener("keyup",function(e){
+            if(e.keyCode == 39){  //right
+                
+                if(position !== 48 && position !== 55 && position && position !== 62){
+                    position++;
+
+                    playerShip.remove()
+                    gridCase[position].appendChild(playerShip);
+                }
+                }else if(e.keyCode == 37 ){  //left
+                    if(position !== 42 && position !== 49 && position && position !== 56){
+                        position--;
+                        playerShip.remove()
+                        gridCase[position].appendChild(playerShip);        
+
+                    }else{position}
+              
+                }else if(e.keyCode ==  38){ //up
+                    if(position > 48){
+                        position-=7;
+                        playerShip.remove()
+                        gridCase[position].appendChild(playerShip);
+                    }
+                }else if(e.keyCode == 40){ //down
+                    
+                    if(position < 56){
+                        position+=7;
+                        playerShip.remove()
+                        gridCase[position].appendChild(playerShip);
+                    }else{
+                        position = position
+                    }
+                }
+        })
+
+    }
+
     handleEnemiesPatern() {
         const gridCase = document.querySelectorAll("#case");
+
+        
         this.patern.forEach(index => {
             let enemyShip = document.createElement('img');
             enemyShip.setAttribute("src", "./ressources/ennemies.png");
+
+         
             gridCase[index + this.mov].appendChild(enemyShip);
+
         })
     }
 
-    handleMovementPatern() {
+    handleMovementPaternRight() {
+        const gridCase = document.querySelectorAll("#case");
+        this.patern.forEach(index => {
+            gridCase[index + this.mov].removeChild(gridCase[index + this.mov].firstChild);
+
+        })
+        if(this.boolRight) {
+            this.mov+=8;
+            console.log("a driote")
+            this.boolRight = false;
+        }
+        
+        this.mov++
+        this.handleEnemiesPatern();
+    }
+
+    handleMovementPaternLeft() {
         const gridCase = document.querySelectorAll("#case");
         this.patern.forEach(index => {
             gridCase[index + this.mov].removeChild(gridCase[index + this.mov].firstChild);
         })
-
-        this.mov++;
+        if(this.boolLeft) {
+            this.mov+=8;
+            this.boolLeft = false;
+        }
+        
+        this.mov--
         this.handleEnemiesPatern();
     }
 }
 
 const initGrid = new GridPatern();
 initGrid.createGrid();
-
+initGrid.handlePlayerPatern();
 
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+let k = 0; 
 async function loop() {
     await sleep(2000);
-    initGrid.handleMovementPatern();
+    if(k<1){
+        initGrid.handleMovementPaternRight();
+    }else{
+        initGrid.handleMovementPaternLeft();
+        k=0
+    }
+    k++;
     loop();
 }
 
