0738de1dc9a20cfb5a1785b1eb221cec5d8e223d
Maghwyn <christopher.samson.pro@gmail.com>
2022-01-07 03:38:52 +0100

    Added some more comments, probably the last update for those.

--- MemoryCard/app.js
+++ MemoryCard/app.js
@@ -4,18 +4,29 @@
 * The game is composed of 12 cards on a 4x3 grid.
 * The player is able to return 2 cards before they're validated or flipped over.
 * Once the player validate every cards, the game end with the time displayed as a result.
+* The player is then asked to submit his/her name to the leaderboard, and reset the game.
 */
 
 class GameResetOption {
     /**
-     * The following class will set-up the two elements needed for the reset option.
-     * We added the ability to toggle the visibility of the button depending on the state of the game.
-     * We also added a way to get the HTMLButtonElement outside of the class.
+     * The following class will set-up all the elements needed for the menu.
+     * We added the ability to toggle the visibility of the menu depending on the state of the game.
+     * We also added a way to get some specific elements outside of the class.
      * 
-     *@property {boolean} elementsCreated => Define when the div and the button are added to the HTML.
-     *@property {HTMLDivElement} menu => Create a div that will contain a a paragraph and a button.
-     *@property {HTMLButtonElement} resetBtn => Create a button with the purpose of resetting the game board.
-     *@property {HTMLButtonElement} fetchResetBtn => Used to store the button so we can do things with it later on.
+     *@property {HTMLDivElement} gameBoard => Fetch the main div containing the game and the menu message.
+     *@property {HTMLDivElement} menu => Create a div that will contain the congrats message, the leaderboard menu and the reset menu.
+     *@property {HTMLParagraphElement} congratsMsg => Create a paragraph congratulating the player for winning with the time elapsed.
+     *@property {HTMLDivElement} userDiv => Create a div that will contain the user message, the username input and the submit button to create a cell.
+     *@property {HTMLParagraphElement} userMsg => Create a paragraph asking the player to enter his/her username.
+     *@property {HTMLInputElement} username => Create a username input that be used to set up the cell username of the leaderboard div score.
+     *@property {HTMLButtonElement} submitBtn => Create a button submitting the username input value to the cell username.
+     *@property {HTMLDivElement} actionDiv => Create a div that will contain a question asking for an action and the reset button.
+     *@property {HTMLParagraphElement} nextActionMsg => Create a paragraph asking for the next action.
+     *@property {HTMLButtonElement} resetBtn => Create a button submitting a reset of the whole game to its origin, beside the leaderboard.
+     *@property {boolean} elementsCreated => Define when all of the elements above are implemented to the HTML.
+     *
+     *@method setUp() => Create a document fragment that will calculate each elements and add them to the gameBoard node.
+     *@method display() => Based on the argument, hide or display the menu message based on the state of the game.
      */
 
     #gameBoard; #menu; #congratsMsg; #userDiv; #userMsg; #username; #submitBtn; #actionDiv; #nextActionMsg; #resetBtn; #elementsCreated;    
@@ -54,6 +65,7 @@ class GameResetOption {
         this.#resetBtn.innerHTML = "Restart";
 
         elements.forEach(el => { el.setAttribute("class", elClasses[count++]); })
+        this.#username.setAttribute("maxlength", "20");
 
         this.#gameBoard.appendChild(documentFragment);
         this.#elementsCreated = true;
@@ -84,12 +96,13 @@ class GameResetOption {
 
 class GameShuffleProcess {
     /**
-     * The following class will set-up a shuffled array based on the ressources at disposal.
+     * The following class will set-up a shuffled random theme array based on the ressources at disposal.
      * Once this array is done being processed, we added a way to extract it from the class.
      * Its use will be to create a pseudo-random game board in the next class.
      * 
      *@property {collection} cardsContent => Create an array of Object Card that will be shuffled later on.
-     *@property {Object} fruitsProperties => Define each and every ressources with its name and icon.
+     *@property {Object} fruitsProperties => Define each and every fruits ressources with their names and icons.
+     *@property {Object} plantsProperties => Define each and every plants ressources with their names and icons.
      */
 
     #cardsContent;
@@ -148,16 +161,20 @@ class GameShuffleProcess {
 class GameCore {
     /**
      * The following class is the main core of the game.
-     * We start by reattributing the attributes of each cards with a pseudo-random pattern from the Shufffle Class.
+     * We start by redefining the attributes of each cards with a pseudo-random pattern from the Shufffle Class.
      * Once this process is done, we finally add the events on each of these cards so we can access the hidden content.
      * We added a limit of 2 actives cards on the board, to avoid spamming and unwanted errors.
      * These 2 actives cards are also compared, if they're equal they stay on the board, otherwise the program flip them again.
-     * Finally, once each cards are flipped over and actives, the game end with a message and the apparition of the reset button.
+     * Finally, once each cards are flipped over and actives, the game end with a message and the apparition of the game menu.
      * 
      *@property {HTMLDivElement} cards => Store each and every parents HTMLDivElement, in other words each cards.
      *@property {HTMLDivElement} cardsSwitch => Store each and every HTMLDivElement toggling the flip between card cover and content.
-     *@property {HTMLDivElement} selectedCard => Used to store the previous flipped card waiting to be compared.
+     *@property {HTMLDivElement} selectedCard => Store the previous flipped card waiting to be compared.
      *@property {integer} cardsActive => The number of cards flipped over which are likely to flip again.
+     *
+     *@method handleCardEvent() => Each click will flip over a card, only two cards can be flipped before being compared.
+     * If the cards are not similar, they're flipped over. If they are we check if all the cards are flipped over, if so we call the menu method.
+     *@method showMenuIfGameEnded => Set up 3 events, the username input, the username submit button and the reset button. Also setting the congrats message.
      */
 
     #cards; #cardsSwitch; #selectedCard; #cardsActive;
@@ -264,10 +281,12 @@ class GameTimer {
      * To be the most precize with the ms, we used Date.now().
      * We also added a way to get the elapsed time between the end of the game and its start.
      * 
-     *@property {integer} elapsedTime => Update the time elapsed between the origin and the call.
+     *@property {integer} elapsedTime => Update the time elapsed between the origin and the method call.
      *@property {Date} dateOrigin => Set-Up the origin time when the game start anew.
      *@property {boolean} isStopped => Toggle the timer to on or off based on the state of the game.
      *@property {*} timerProcess => Used to trigger the timer process.
+     *
+     *@method runningTimer() => Recursive method for the timer cancelling itself at each iteration.
      */
 
     #elapsedTime; #dateOrigin; #isStopped; #timerProcess;
@@ -306,6 +325,22 @@ class GameTimer {
 }
 
 class GameLeaderboard {
+    /**
+     * The following class will set-up a cell in the score leaderboard with a name and the elapsed time.
+     * We added a way to add a cell to the score leaderboard in the first place.
+     * Then we added a way to sort the score if one of them got a quicker run.
+     * Once a score is submitted, the button will cease to function to avoid spamming.
+     * 
+     *@property {HTMLDivElement} scoreTab => The div element where all the score cells are created.
+     *@property {HTMLDivElement} newCell => Store the new score cell to be used outside the class.
+     *@property {string} username => The name of the player, if submitted.
+     *@property {boolean} wasScoreAdded => If a score was added, this value will deactivate the event on the submit button.
+     *
+     *@method addScore() => Add a cell to the score leaderboard.
+     *@method sortScore() => Sort all the cells inside the score leaderboard so it goes from quickest to slowest.
+     *@method wasScoreAdded() => Toggle the boolean to true when a score is added, to false when the reset happen.
+     */
+
     #scoreTab; #newCell; #username; #wasScoreAdded;
 
     constructor() {
@@ -382,7 +417,7 @@ class GameLeaderboard {
 
 function StartGameBoard(state) {
     /**
-     * The following function will set-up the whole game in differents but required aspect so an user can start playing.
+     * The following function will set-up the whole game in differents but required aspects so an user can start playing.
      *@param {string} state => This variable will be used to differentiate a reset from the first launch.
      */
 
